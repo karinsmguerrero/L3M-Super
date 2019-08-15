3b791a8888d2636a49904fc26dcfc1dff07160b5
Gerald Valverde <gerald-_16@hotmail.com>
2019-08-15 00:06:53 -0600

    API

---
+++ WebApi/WebApi/Controllers/RoleController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class RoleController : ApiController
+    {
+        // GET: api/Role
+        public IEnumerable<Role> Get()
+        {
+            var list = new List<Role>
+            {
+                new Role() { Name = "Administrador" , Description = "Lorem ipsum"},
+                new Role() { Name = "Vendedor" , Description = "Lorem ipsum" },
+                new Role() { Name = "Gerente" , Description = "Lorem ipsum" },
+                new Role() { Name = "Cajero" , Description = "Lorem ipsum" }
+            };
+            return list;
+        }
+
+        // GET: api/Role/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Role
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Role/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Role/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApi/WebApi/Controllers/SchedulesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class SchedulesController : ApiController
+    {
+        // GET: api/Schedules
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Schedules/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Schedules
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Schedules/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Schedules/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
