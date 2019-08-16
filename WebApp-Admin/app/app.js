4a43aed77954a429fe49ec0c3b5a3e1ad1a77e6a
Dario <darior1227@gmail.com>
2019-08-16 01:52:07 -0600

    zx

--- WebApp-Admin/app/app.js
+++ WebApp-Admin/app/app.js
@@ -1,53 +1,57 @@
- var superApp = angular.module('superApp',['ngRoute','ngAnimate']);
+var superApp = angular.module('superApp', ['ngRoute', 'ngAnimate']);
 /**
 * Run before the app Start
 * Rutas de las pafinas
 **/
-superApp.config(['$routeProvider',function($routeProvider){
+superApp.config(['$routeProvider', function ($routeProvider) {
   $routeProvider
-  .when('/home', {
-    templateUrl: 'views/home.html',
-    controller:'SuperController'
-  })
-  .when ('/horario', {
-    templateUrl: 'views/horario.html',
-    controller:'HorarioController'
-  })
-  .when('/login-success', {
-    templateUrl: 'views/login-success.html',
-    controller:'LoginController'
-  })
-  .when('/login', {
-    templateUrl: 'views/login.html',
-    controller:'LoginController'
-  })
-  .when('/a', {
-    templateUrl: 'views/hora.html',
-    controller:'HorarioController'
-  })
-  .when('/roles',{
-    templateUrl: 'views/roles.html',
-    controller:'SuperController'
-  })
-  .when('/sucursales',{
-    templateUrl: 'views/sucursales.html',
-    controller:'SucursalesController'
-  })
-  .when('/proveedores',{
-    templateUrl: 'views/proveedores.html',
-    controller:'ProveedoresController'
-  })
-  .when('/trabajadores',{
-  templateUrl: 'views/trabajadores.html',
-  controller:'TrabajadoresController'
-})
-.when('/productos', {
-  templateUrl: 'views/productos.html',
-  controller:'ProductosController'
-})
-  .otherwise({
-    redirectTo:'/home'
-  })
+    .when('/home', {
+      templateUrl: 'views/home.html',
+      controller: 'SuperController'
+    })
+    .when('/horario', {
+      templateUrl: 'views/horario.html',
+      controller: 'HorarioController'
+    })
+    .when('/login-success', {
+      templateUrl: 'views/login-success.html',
+      controller: 'LoginController'
+    })
+    .when('/login', {
+      templateUrl: 'views/login.html',
+      controller: 'LoginController'
+    })
+    .when('/a', {
+      templateUrl: 'views/hora.html',
+      controller: 'HorarioController'
+    })
+    .when('/roles', {
+      templateUrl: 'views/roles.html',
+      controller: 'SuperController'
+    })
+    .when('/sucursales', {
+      templateUrl: 'views/sucursales.html',
+      controller: 'SucursalesController'
+    })
+    .when('/proveedores', {
+      templateUrl: 'views/proveedores.html',
+      controller: 'ProveedoresController'
+    })
+    .when('/trabajadores', {
+      templateUrl: 'views/trabajadores.html',
+      controller: 'TrabajadoresController'
+    })
+    .when('/productos', {
+      templateUrl: 'views/productos.html',
+      controller: 'ProductosController'
+    })
+    .when('/compras', {
+      templateUrl: 'views/compras.html',
+      controller: 'ComprasController'
+    })
+    .otherwise({
+      redirectTo: '/home'
+    })
 
 
 
@@ -57,17 +61,17 @@ superApp.config(['$routeProvider',function($routeProvider){
 *Run when app is running
 **/
 superApp.run(function () {
-/*  $scope.navs=[{
-    url:'/nav-mobile.html'
-  },{
-    url:'/nav-bar.html'
-  }];
-  $scope.nav=$scope.navs[1];
-  var nua = navigator.userAgent;
-  var isAndroid = (nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1 && nua.indexOf('Chrome') === -1)
-  if (isAndroid) {
-    $scope.nav=$scope.navs[0];
-  };*/
+  /*  $scope.navs=[{
+      url:'/nav-mobile.html'
+    },{
+      url:'/nav-bar.html'
+    }];
+    $scope.nav=$scope.navs[1];
+    var nua = navigator.userAgent;
+    var isAndroid = (nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1 && nua.indexOf('Chrome') === -1)
+    if (isAndroid) {
+      $scope.nav=$scope.navs[0];
+    };*/
 });
 
 /**
@@ -76,7 +80,7 @@ superApp.run(function () {
 */
 superApp.controller('SuperController', ['$scope', '$http', function ($scope, $http) {
 
-  $scope.eliminarRol= function (rol) {
+  $scope.eliminarRol = function (rol) {
     var posRol = $scope.roles.indexOf(rol);
     $scope.roles.splice(posRol, 1);
   };
@@ -86,17 +90,17 @@ superApp.controller('SuperController', ['$scope', '$http', function ($scope, $ht
       nombre: $scope.nrol.nombre,
       descripcion: $scope.nrol.descripcion
     });
-    $scope.nrol.nombre="";
-    $scope.nrol.descripcion=""
+    $scope.nrol.nombre = "";
+    $scope.nrol.descripcion = ""
   };
 
-  $scope.removeAll = function(){
+  $scope.removeAll = function () {
     $scope.roles = []
   };
 
 
-  $http.get('data/roles.json').then(function(data){
-    $scope.roles=data.data;
+  $http.get('data/roles.json').then(function (data) {
+    $scope.roles = data.data;
   });
 }]);
 
@@ -106,7 +110,7 @@ superApp.controller('SuperController', ['$scope', '$http', function ($scope, $ht
 *
 */
 superApp.controller('TrabajadoresController', ['$scope', '$http', function ($scope, $http) {
-  $scope.eliminarTrabajador =  function (trabajador) {
+  $scope.eliminarTrabajador = function (trabajador) {
     var posTrab = $scope.trabajadores.indexOf(trabajador);
     $scope.trabajadores.splice(posTrab, 1);
   };
@@ -128,11 +132,11 @@ superApp.controller('TrabajadoresController', ['$scope', '$http', function ($sco
     $scope.ntrab.salarioPH = ""
   };
 
-  $scope.removeAll = function() {
+  $scope.removeAll = function () {
     $scope.trabajadores = []
   };
 
-  $http.get('data/trabajadores.json').then(function(data) {
+  $http.get('data/trabajadores.json').then(function (data) {
     $scope.trabajadores = data.data;
   });
 }]);
@@ -144,7 +148,7 @@ superApp.controller('TrabajadoresController', ['$scope', '$http', function ($sco
 */
 superApp.controller('ProveedoresController', ['$scope', '$http', function ($scope, $http) {
 
-  $scope.eliminarProveedor= function (proveedor) {
+  $scope.eliminarProveedor = function (proveedor) {
     var posProv = $scope.proveedores.indexOf(proveedor);
     $scope.proveedores.splice(posProv, 1);
   };
@@ -154,20 +158,96 @@ superApp.controller('ProveedoresController', ['$scope', '$http', function ($scop
       nombre: $scope.nprov.nombre,
       cedula: $scope.nprov.cedula
     });
-    $scope.nprov.nombre="";
-    $scope.nprov.cedula=""
+    $scope.nprov.nombre = "";
+    $scope.nprov.cedula = ""
   };
 
-  $scope.removeAll = function(){
+  $scope.removeAll = function () {
     $scope.proveedores = []
   };
 
 
-  $http.get('data/proveedores.json').then(function(data){
-    $scope.proveedores=data.data;
+  $http.get('data/proveedores.json').then(function (data) {
+    $scope.proveedores = data.data;
   });
 }]);
 
+/**
+*Controlador de la vista de gestion de
+*productos
+*
+*/
+superApp.controller('ProductosController', ['$scope', '$http', function ($scope, $http) {
+
+  $scope.eliminarProducto = function (producto) {
+    var posProd = $scope.productos.indexOf(producto);
+    $scope.productos.splice(posProd, 1);
+  };
+
+  $scope.addProducto = function () {
+    $scope.productos.push({
+      nombre: $scope.nprod.nombre,
+      descripcion: $scope.nprod.descripcion,
+      codigoBarras: $scope.nprod.codigoBarras,
+      proveedor: $scope.nprod.proveedor,
+      precioCompra: $scope.nprod.precioCompra,
+      impuesto: $scope.nprod.impuesto,
+      descuento: $scope.nprod.descuento,
+    });
+    $scope.nprod.nombre = "";
+    $scope.nprod.descripcion = "";
+    $scope.nprod.codigoBarras = "";
+    $scope.nprod.proveedor = "";
+    $scope.nprod.precioCompra = "";
+    $scope.nprod.impuesto = "";
+    $scope.nprod.descuento = "";
+  };
+
+  $scope.removeAll = function () {
+    $scope.productos = []
+  };
+
+
+  $http.get('data/productos.json').then(function (data) {
+    $scope.productos = data.data;
+  });
+}]);
+
+/**
+*Controlador de la vista de gestion de las compras
+*
+*/
+superApp.controller('ComprasController', ['$scope', '$http', function ($scope, $http) {
+
+  $scope.eliminarCompra = function (compra) {
+    var posComp = $scope.compras.indexOf(compra);
+    $scope.compras.splice(posComp, 1);
+  };
+
+  $scope.addCompra = function () {
+    $scope.compras.push({
+      descripcion: $scope.ncomp.descripcion,
+      fechaDeCompra: $scope.ncomp.fechaDeCompra,
+      fechaRegistroCompra: $scope.ncomp.fechaRegistroCompra,
+      proveedor: $scope.ncomp.proveedor,
+      sucursalCompra: $scope.ncomp.sucursalCompra,
+    });
+    $scope.ncomp.descripcion = "";
+    $scope.ncomp.fechaDeCompra = "";
+    $scope.ncomp.fechaRegistroCompra = "";
+    $scope.ncomp.proveedor = "";
+    $scope.ncomp.sucursalCompra = "";
+  };
+
+  $scope.removeAll = function () {
+    $scope.compras = []
+  };
+
+
+  $http.get('data/compras.json').then(function (data) {
+    $scope.compras = data.data;
+  });
+}]);
 
 /**
 * Controlador de la vista de horarios
@@ -175,7 +255,7 @@ superApp.controller('ProveedoresController', ['$scope', '$http', function ($scop
 */
 superApp.controller('HorarioController', ['$scope', '$http', function ($scope, $http) {
 
-  $scope.eliminarHorario= function (rol) {
+  $scope.eliminarHorario = function (rol) {
     var poshor = $scope.horario.indexOf(rol);
     $scope.horario.splice(poshor, 1);
   };
@@ -187,20 +267,20 @@ superApp.controller('HorarioController', ['$scope', '$http', function ($scope, $
       horas: $scope.nhor.horas,
       sucursal: $scope.nhor.sucursal,
     });
-    $scope.nhor.nombre="";
-    $scope.nhor.semana="";
-    $scope.nhor.horas="";
-    $scope.nhor.sucursal="";
+    $scope.nhor.nombre = "";
+    $scope.nhor.semana = "";
+    $scope.nhor.horas = "";
+    $scope.nhor.sucursal = "";
   };
 
-  $http.get('data/horario.json').then(function(data){
-    $scope.horario=data.data;
+  $http.get('data/horario.json').then(function (data) {
+    $scope.horario = data.data;
   });
 }]);
 
 superApp.controller('SucursalesController', ['$scope', '$http', function ($scope, $http) {
 
-  $scope.eliminarSucursal= function (rol) {
+  $scope.eliminarSucursal = function (rol) {
     var posSuc = $scope.sucursales.indexOf(rol);
     $scope.sucursales.splice(posSuc, 1);
   };
@@ -213,51 +293,51 @@ superApp.controller('SucursalesController', ['$scope', '$http', function ($scope
       telefono: $scope.nsuc.telefono,
       administrador: $scope.nsuc.administrador,
     });
-    $scope.nsuc.nombre="";
-    $scope.nsuc.direccion="";
-    $scope.nsuc.telefono="";
-    $scope.nsuc.administrador="";
+    $scope.nsuc.nombre = "";
+    $scope.nsuc.direccion = "";
+    $scope.nsuc.telefono = "";
+    $scope.nsuc.administrador = "";
   };
 
-  $http.get('data/sucursales.json').then(function(data){
-    $scope.sucursales=data.data;
+  $http.get('data/sucursales.json').then(function (data) {
+    $scope.sucursales = data.data;
   });
 }]);
 
-superApp.controller('LoginController', ['$scope','$location',function ($scope,$location) {
-  $scope.sendMessage= function () {
+superApp.controller('LoginController', ['$scope', '$location', function ($scope, $location) {
+  $scope.sendMessage = function () {
     $location.path('/login-success');
 
   };
 }]);
 
 
-superApp.controller('NavController', ['$scope',function ($scope) {
+superApp.controller('NavController', ['$scope', function ($scope) {
 
-    $scope.path=function () {
-      var nua = navigator.userAgent;
-      var isAndroid = (nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1 && nua.indexOf('Chrome') === -1)
-      if (isAndroid) {
-        $scope.path.src="'nav-mobile.html'";
-      }else{
-        $scope.path.src="'nav-bar.html'";
-      }
-    };
+  $scope.path = function () {
+    var nua = navigator.userAgent;
+    var isAndroid = (nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1 && nua.indexOf('Chrome') === -1)
+    if (isAndroid) {
+      $scope.path.src = "'nav-mobile.html'";
+    } else {
+      $scope.path.src = "'nav-bar.html'";
+    }
+  };
 
 
 }]);
 
 function NavController() {
-  $scope.navs=[{
-    url:'nav-mobile.html'
-  },{
-    url:'nav-bar.html'
+  $scope.navs = [{
+    url: 'nav-mobile.html'
+  }, {
+    url: 'nav-bar.html'
   }];
-  $scope.nav=$scope.navs[1];
+  $scope.nav = $scope.navs[1];
   var nua = navigator.userAgent;
   var isAndroid = (nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1 && nua.indexOf('Chrome') === -1)
   if (isAndroid) {
-    $scope.nav=$scope.navs[0];
+    $scope.nav = $scope.navs[0];
   };
 
 }
