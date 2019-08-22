2e1464886cbb386737c3a1ff43eb94dc0bfa16c6
Karina Martinez <karinamguerrero98@gmail.com>
2019-08-22 10:51:07 -0600

    bb

--- WebApi/WebApi/Controllers/SchedulesController.cs
+++ WebApi/WebApi/Controllers/SchedulesController.cs
@@ -4,15 +4,53 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Xml;
+using WebApi.Miscellaneous;
+using WebApi.Models;
 
 namespace WebApi.Controllers
 {
     public class SchedulesController : ApiController
     {
+        private string xmlPath = Constants.XML_PATH + "WeeklySchedule.xml";
+
         // GET: api/Schedules
-        public IEnumerable<string> Get()
+        public IEnumerable<WorkWeek> Get()
         {
-            return new string[] { "value1", "value2" };
+            XmlDocument xml = new XmlDocument();
+            xml.Load(xmlPath);
+
+            var list = new List<WorkWeek>();
+            XmlNodeList schedule = xml.DocumentElement.SelectNodes("/schedule/week");
+
+            foreach (XmlNode week in schedule)
+            {
+                string startDay = week.Attributes["start"].Value;
+                string endDay = week.Attributes["end"].Value;
+
+                XmlNodeList employees = week.ChildNodes;
+
+                foreach (XmlNode employee in employees)
+                {
+                    string name = employee.Attributes["name"].Value;
+                    string hours = employee["hours"].InnerText;
+                    string extras = employee["extras"].InnerText;
+                    string branch = employee["branch"].InnerText;
+
+                    list.Add(new WorkWeek()
+                    {
+                        StartDay = startDay,
+                        EndDay = endDay,
+                        Branch = branch,
+                        EmployeeName = name,
+                        Hours = hours,
+                        ExtraHours = extras
+                    });
+                }
+                                
+            }
+
+            return list;
         }
 
         // GET: api/Schedules/5
@@ -22,8 +60,47 @@ namespace WebApi.Controllers
         }
 
         // POST: api/Schedules
-        public void Post([FromBody]string value)
+        public void Post([FromBody]WorkWeek value)
         {
+            XmlDocument xml = new XmlDocument();
+            xml.Load(xmlPath);
+
+            XmlNode schedule = xml.DocumentElement.SelectSingleNode("/schedule");
+
+            XmlNode newWeek = xml.CreateElement("week");
+
+            // Create attributes for role and append them to the role element.
+            XmlAttribute attribute = xml.CreateAttribute("start");
+            attribute.Value = value.StartDay;
+            newWeek.Attributes.Append(attribute);
+
+            attribute = xml.CreateAttribute("end");
+            attribute.Value = value.EndDay;
+            newWeek.Attributes.Append(attribute);
+
+            XmlNode employee = xml.CreateElement("employee");
+     
+            attribute = xml.CreateAttribute("name");
+            attribute.Value = value.EmployeeName;
+            employee.Attributes.Append(attribute);
+
+            // Create and append a child element for the description of the role.
+            XmlNode hours = xml.CreateElement("hours");
+            hours.InnerText = value.Hours;
+            employee.AppendChild(hours);
+
+            XmlNode extras = xml.CreateElement("extras");
+            extras.InnerText = value.ExtraHours;
+            employee.AppendChild(extras);
+
+            XmlNode branch = xml.CreateElement("branch");
+            branch.InnerText = value.Branch;
+            employee.AppendChild(branch);
+
+            newWeek.AppendChild(employee);
+            schedule.AppendChild(newWeek);
+
+            xml.Save(xmlPath);
         }
 
         // PUT: api/Schedules/5
---
+++ WebApi/WebApi/Models/WorkWeek.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class WorkWeek
+    {
+        public string EmployeeName { get; set; }
+        public string StartDay { get; set; }
+        public string EndDay { get; set; }
+        public string Hours { get; set; }
+        public string Branch { get; set; }
+        public string ExtraHours { get; set; }
+    }
+}
