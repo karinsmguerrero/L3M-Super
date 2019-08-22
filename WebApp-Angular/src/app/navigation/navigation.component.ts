46a97ce3fa727587d2c8aa817f15a063eb04cb4d
geraldvm <gerald-_16@hotmail.com>
2019-08-22 06:02:38 -0600

    Automatizar Navbar

--- WebApp-Angular/src/app/navigation/navigation.component.ts
+++ WebApp-Angular/src/app/navigation/navigation.component.ts
@@ -3,38 +3,73 @@ import { DeviceDetectorService } from 'ngx-device-detector';
 import {  HttpClient } from "@angular/common/http";
 import { NavbarManagementService } from 'src/app/services/navbar-management.service';
 import { Nav } from 'src/app/models/nav.model';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 
 })
+/**
+ * Componente de navegacion
+ * Clase encargada de mostrar
+ * los diferentes NavBbar
+ */
 export class NavigationComponent implements OnInit {
   deviceInfo = null;
-  mobile_class: string;
-  administration_class:string;
+  isDesktopDevice = null;
+  isMobile = null;
+  isAdmin:boolean;
+  isReport:boolean;
+  isSale:boolean;
+  route: Array<String>;
     
-    constructor(private http : HttpClient, private deviceService: DeviceDetectorService) {
-      this.mobileFunction();
+    constructor(private http : HttpClient, 
+      private deviceService: DeviceDetectorService,
+      private router: Router,
+      private location: Location) {
+        router.events.subscribe((val) => {
+          if(location.path() != ''){
+            this.route = location.path().split("/");//Verifica cual vista se desea ver, para asi modificar el Navbar
+            if(this.route[1]=="Administracion"){
+              this.isAdmin=true;
+              this.isReport=false;
+              this.isSale=false;
+            }if(this.route[1]=="Reportes"){
+              this.isAdmin=false;
+              this.isReport=true;
+              this.isSale=false;
+            }if(this.route[1]=="Ventas"){
+              this.isAdmin=false;
+              this.isReport=false;
+              this.isSale=true;
+            }
+            //console.log(this.route[1]=="Administracion");
+            //console.log(this.route);
+          } else {
+            this.isAdmin=true;
+            this.isReport=false;
+            this.isSale=false;
+          }
+        });
   }
+  /**
+   * Metodo encargado de decidir si 
+   * se observa desde un dispositivo movil
+   */
   mobileFunction() {
-    console.log('hello `Home` component');
     this.deviceInfo = this.deviceService.getDeviceInfo();
-    const isMobile = this.deviceService.isMobile();
-    const isTablet = this.deviceService.isTablet();
-    const isDesktopDevice = this.deviceService.isDesktop();
-    console.log(this.deviceInfo);
-    console.log(isMobile);  // returns if the device is a mobile device (android / iPhone / windows-phone etc)
-    console.log(isTablet);  // returns if the device us a tablet (iPad etc)
-    console.log(isDesktopDevice); // returns if the app is running on a Desktop browser.
-
+    this.isMobile = this.deviceService.isMobile();//Retorna true si es un dispositivo movil
+    this.isDesktopDevice = this.deviceService.isDesktop();//Retorna false si se accede desde una computadora
   }
 
-  selectNavFunction(nav : Nav){
-  }
 
   ngOnInit() {
-    //this.service.getNav();
+    this.mobileFunction();
+
   }
 
 }
--- WebApp-Angular/src/app/reports/product-list/product-list.component.ts
+++ WebApp-Angular/src/app/reports/product-list/product-list.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsManagmentService } from 'src/app/services/products-managment.service';
 import { Product } from '../../models/product.model';
-
+import {ActivatedRoute} from '@angular/router';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-
+  sucursal = '';
   readonly rootURL = "http://localhost:59791/api";
   productList : Product[];
-  constructor( private service : ProductsManagmentService) { }
-
+  constructor( private service : ProductsManagmentService, 
+    private route: ActivatedRoute) { }
+    
   ngOnInit() {
     this.service.getProducts();
+    this.sucursal= this.route.snapshot.params.sucursal;//Revisar.params
+    console.log(this.route.root.toString);
+
   }
 
   populateForm(product : Product){
